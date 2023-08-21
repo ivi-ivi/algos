3d81a6ba341a3f8909ab684d2f9ea68646b5d872
Иванова Вероника Владимировна <VVlaIvanova@sberbank.ru>
2023-08-21 21:11:55 +0300

    6

--- secondSort.js
+++ secondSort.js
@@ -1,21 +1,27 @@
 const secondSort = (arr) => {
     let res = [0]
     let min = Infinity
-    let repeats = 0
+    let repeats = -1
     for (let j = 0; j < arr.length; j++) {
         for (let i = 0; i < arr.length; i++) {
             if (arr[i] < min && res[res.length - 1] === arr[i]) {
-
+                repeats++
             }
             if (arr[i] < min && res[res.length - 1] < arr[i]) {
                 min = arr[i]
             }
         }
+        let toRepeat = res[res.length - 1]
+        while (repeats) {
+            res.push(toRepeat)
+            repeats--
+        }
         if (min !== Infinity)
             res.push(min)
         min = Infinity
+        repeats = -1
     }
     return res
 }
 
-secondSort([1, 7, 6, 3, 0])
+secondSort([1, 1, 7, 6, 3, 0])
