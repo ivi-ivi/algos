ac4e58da8c4c2d81decb39c1e62eb97838eb548e
Иванова Вероника Владимировна <VVlaIvanova@sberbank.ru>
2023-08-21 21:34:27 +0300

    7

--- secondSort.js
+++ secondSort.js
@@ -1,27 +1,32 @@
 const secondSort = (arr) => {
-    let res = [0]
-    let min = Infinity
-    let repeats = -1
+    const res = [];
+    let min = Infinity;
+    let repeats = -1;
+
     for (let j = 0; j < arr.length; j++) {
         for (let i = 0; i < arr.length; i++) {
-            if (arr[i] < min && res[res.length - 1] === arr[i]) {
-                repeats++
+            if (arr[i] < min && (res.length === 0 || res[res.length - 1] === arr[i])) {
+                repeats++;
             }
-            if (arr[i] < min && res[res.length - 1] < arr[i]) {
-                min = arr[i]
+            if (arr[i] < min && (res.length === 0 || res[res.length - 1] < arr[i])) {
+                min = arr[i];
             }
         }
-        let toRepeat = res[res.length - 1]
+        let toRepeat = res.length > 0 ? res[res.length - 1] : null;
         while (repeats) {
-            res.push(toRepeat)
-            repeats--
+            if (toRepeat !== null)
+                res.push(toRepeat);
+            repeats--;
         }
-        if (min !== Infinity)
-            res.push(min)
-        min = Infinity
-        repeats = -1
+
+        if (min !== Infinity) {
+            res.push(min);
+        }
+        min = Infinity;
+        repeats = -1;
     }
+
     return res
 }
 
-secondSort([1, 1, 7, 6, 3, 0])
+secondSort([1, 1, 7, -6, -6, -6, 3, 0]);
