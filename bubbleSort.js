fda2611269c9a7fe80283693ca0ab230b9ed8c2c
Иванова Вероника Владимировна <VVlaIvanova@sberbank.ru>
2023-08-21 18:35:29 +0300

    1

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,7 +1,7 @@
 const bubbleSort = (arr) => {
-    for (let i = 0; i < arr.length - 1; i++) {
-        for (let j = 1; j < arr.length; j++) {
-            if (arr[i] > arr[j]) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[i] < arr[j]) {
                 let tmp = arr[i];
                 arr[i] = arr[j]
                 arr[j] = tmp
